096caa55d09838a60b0545e8b9794527b6ba73b8
previn0002 <previn0002@gmail.com>
2026-04-25 12:25:22 +0400

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'gts-ttr-v1';
+const urlsToCache = ['/'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
